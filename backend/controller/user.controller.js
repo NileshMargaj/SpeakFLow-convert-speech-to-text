4749c0abab20fcbcfec26f3d67562246a4d91395
NileshMargaj <nileshmargaj751@gmail.com>
2026-05-30 17:49:42 +0530

    cookie fixed

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -40,6 +40,7 @@ export const registerUser = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             maxAge: 30 * 24 * 60 * 60 * 1000
         })
 
@@ -120,6 +121,7 @@ export const loginUser = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
             maxAge: 30 * 24 * 60 * 60 * 1000
         })
 
