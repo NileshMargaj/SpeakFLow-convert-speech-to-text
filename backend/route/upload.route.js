49dd564cc319cfd589a0e224dae8cf60f5470986
NileshMargaj <nileshmargaj751@gmail.com>
2026-05-30 11:33:42 +0530

    upload-api fixed

--- backend/route/upload.route.js
+++ backend/route/upload.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { isUserAuthenticated } from "../middleware/auth.Middleware.js";
+import { isUserAuthenticated } from "../middleware/auth.middleware.js";
 import upload from "../middleware/upload.Middleware.js";
 import handleMulterError from "../middleware/multerError.middleware.js";
 import { uploadAudio, } from "../controller/upload.controller.js";
