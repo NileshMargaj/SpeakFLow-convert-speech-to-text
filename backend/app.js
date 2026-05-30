77e5f747e3b0d5ff4de9226954ffdbc021425a4c
NileshMargaj <nileshmargaj751@gmail.com>
2026-05-30 11:27:57 +0530

    app err fix

--- backend/app.js
+++ backend/app.js
@@ -24,6 +24,7 @@ app.use(cors({
         process.env.CLIENT_URL,
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        process.env.FRONTEND_URL
     ].filter(Boolean),
     credentials: true,
 }))
